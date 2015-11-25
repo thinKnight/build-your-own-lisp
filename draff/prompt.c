c693b59074127be9f248b00ef411b63176cc1d04
thinKnight <jeeyeyeah@gmail.com>
2015-11-26 00:37:19 +0800

    first commit

--- draff/prompt.c
+++ draff/prompt.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdio.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
 
 /* Declare a buffer for user input of size 2048 */
-static char input[2048];
+/* static char input[2048]; */
 
 int main(int argc, char** argv) {
 
@@ -13,13 +17,22 @@ int main(int argc, char** argv) {
     while(1) {
 
 	    /* Output our prompt */
-        fputs("lispy> ", stdout);
+        /* fputs("lispy> ", stdout); */
 
         /* Read a line of user input of maximum size 2048 */
-        fgets(input, 2048, stdin);
+        /* fgets(input, 2048, stdin); */
+
+        /* Output our prompt and get input */
+        char* input = readline("lisp> ");
+
+        /* Add input to history */
+        add_history(input);
 
         /* Echo input back to user */
         printf("No you're a %s", input);
+
+        /* Free retrieved input */
+        free(input);
     }
 
     return 0;
