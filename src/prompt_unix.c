d2f4d8a1909bbd9b2232e1b50f0c142b58af69ce
thinKnight <jeeyeyeah@gmail.com>
2015-11-26 19:41:44 +0800

    daily commit

--- src/prompt_unix.c
+++ src/prompt_unix.c
@@ -1,40 +1,31 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 #include <editline/readline.h>
 #include <editline/history.h>
-
-/* Declare a buffer for user input of size 2048 */
-/* static char input[2048]; */
 
 int main(int argc, char** argv) {
 
     /* Print Version and Exit Information */
     puts("Lispy Version 0.0.0.0.1");
-    puts("Press Ctrl+c to Exit\n");
+    puts("Press ctrl+c to Exit\n");
 
     /* In a never ending loop */
     while(1) {
 
-	    /* Output our prompt */
-        /* fputs("lispy> ", stdout); */
-
-        /* Read a line of user input of maximum size 2048 */
-        /* fgets(input, 2048, stdin); */
-
         /* Output our prompt and get input */
-        char* input = readline("lisp> ");
+        char* input = readline("lispy> ");
 
         /* Add input to history */
         add_history(input);
 
         /* Echo input back to user */
-        printf("No you're a %s", input);
+        printf("No you're a %s\n", input);
 
-        /* Free retrieved input */
+        /* Free retrived input */
         free(input);
     }
 
     return 0;
+
 }
-     
